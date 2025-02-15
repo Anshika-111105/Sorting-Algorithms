650b8dc4b6fadcb65d288d7c2964f7a19e2db66e
Anshika Saklani <166739901+Anshika-111105@users.noreply.github.com>
2025-02-15 09:55:02 +0530

    Update selectionsort.cpp

--- selectionsort.cpp
+++ selectionsort.cpp
@@ -1,4 +1,3 @@
-//time complexity=n^2
 /*Insertion sort is a simple sorting algorithm that works by iteratively inserting each element of an unsorted list into its correct
  position in a sorted portion of the list. It is a stable sorting algorithm, meaning that elements 
  with equal values maintain their relative order in the sorted output.*/
