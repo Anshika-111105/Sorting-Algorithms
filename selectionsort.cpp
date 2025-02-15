ebde0e44aec339db9592522503b924e50a8f1b1e
Anshika Saklani <166739901+Anshika-111105@users.noreply.github.com>
2025-02-15 09:51:43 +0530

    Add files via upload

---
+++ selectionsort.cpp
@@ -0,0 +1,35 @@
+//time complexity=n^2
+/*Insertion sort is a simple sorting algorithm that works by iteratively inserting each element of an unsorted list into its correct
+ position in a sorted portion of the list. It is a stable sorting algorithm, meaning that elements 
+ with equal values maintain their relative order in the sorted output.*/
+#include <bits/stdc++.h>
+using namespace std;
+void selectionSort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        int min_idx = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[min_idx])
+                min_idx = j;
+        }
+        if (min_idx != i)
+            swap(arr[min_idx], arr[i]);
+    }
+}
+void printArray(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+int main()
+{
+    int arr[] = {-64, 25, 12, -22, 11};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    selectionSort(arr, n);
+    cout << "Sorted array: \n";
+    printArray(arr, n);
+    return 0;
+}
