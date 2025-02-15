5641f23fd82244594ca5888609b9f86af851630f
Anshika Saklani <166739901+Anshika-111105@users.noreply.github.com>
2025-02-15 09:31:55 +0530

    Add files via upload

---
+++ insertionsort.cpp
@@ -0,0 +1,38 @@
+//Time complexity=n^2
+/*Selection sort is a simple and efficient sorting algorithm that works by repeatedly selecting
+ the smallest (or largest) element from the unsorted portion of the list and moving it to the sorted portion of the list. 
+*/
+#include <iostream>
+using namespace std;
+void insertionSort(int arr[], int n)
+{
+    for (int i = 1; i < n; ++i) {
+        int key = arr[i];
+        int j = i - 1;
+
+        /* Move elements of arr[0..i-1], that are
+           greater than key, to one position ahead
+           of their current position */
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+void printArray(int arr[], int n)
+{
+    for (int i = 0; i < n; ++i)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+int main()
+{
+    int arr[] = { 12, 11, 13, 5, 6 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    insertionSort(arr, n);
+    printArray(arr, n);
+
+    return 0;
+}
